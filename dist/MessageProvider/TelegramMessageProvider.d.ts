df025d202905e4283525b1945e8959688540c251
modifier <the.modifikator@gmail.com>
2020-02-19 23:49:56 +0300

    Update telegram provider dist code

--- dist/MessageProvider/TelegramMessageProvider.d.ts
+++ dist/MessageProvider/TelegramMessageProvider.d.ts
@@ -1,5 +1,8 @@
 import { MessageProvider } from './MessageProvider';
 import { TelegramMessageResponse } from './TelegramMessageResponse';
 export declare class TelegramMessageProvider implements MessageProvider {
+    private readonly id;
+    private readonly token;
+    constructor(id: number, token: string);
     respond(response: TelegramMessageResponse): Promise<any>;
 }
--- dist/MessageProvider/TelegramMessageProvider.js
+++ dist/MessageProvider/TelegramMessageProvider.js
@@ -2,8 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios = require('axios');
 class TelegramMessageProvider {
+    constructor(id, token) {
+        this.id = id;
+        this.token = token;
+    }
     respond(response) {
-        return axios.post('https://api.telegram.org/bot1071083747:AAEBdDMkL_j7AWacabDHzYv6B7n970FP_to/sendMessage', response);
+        return axios.post(`https://api.telegram.org/bot${this.id}:${this.token}/sendMessage`, response);
     }
 }
 exports.TelegramMessageProvider = TelegramMessageProvider;
